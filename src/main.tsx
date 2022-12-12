25ce8a8bbb4154c76b87773aae01d200590047c0
Rian Lucas <rianluccas08@gmail.com>
2022-12-12 19:26:58 -0300

    Forma mais simples de Fazer requisição no GraphQL

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import './styles/global.css'
+import { ApolloProvider } from '@apollo/client/react'
+import { client } from './lib/apollo'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
