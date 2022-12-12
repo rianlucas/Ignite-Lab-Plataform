ffe1da8577a0ef644de1f357f6551fcf04f7308a
Rian Lucas <rianluccas08@gmail.com>
2022-12-12 18:39:49 -0300

    Hello World

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
+  )
+}
+ 
+export default App
