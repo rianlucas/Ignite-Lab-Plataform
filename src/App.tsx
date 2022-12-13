7ab0bb950edb00aff6e54562e618252515b85f59
Rian Lucas <rianluccas08@gmail.com>
2022-12-12 21:16:21 -0300

    Criando Components e excluindo requisição da GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
 import { gql, useQuery } from '@apollo/client'
 import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
+import { Header } from './components/Header'
+import { Lesson } from './components/Lesson'
+import { Sidebar } from './components/Sidebar'
+import { Video } from './components/Video'
 import { client } from './lib/apollo'
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id 
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string,
-  title: string
-}
-
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY)
-
   return (
-    <ul>
-      {data?.lessons.map((lesson: Lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Header/>
+      <Sidebar/>
+      <Lesson/>
+      <Video/>
+    </div>
   )
 }
  
