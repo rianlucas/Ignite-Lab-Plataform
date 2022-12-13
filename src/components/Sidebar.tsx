ede948b7b82a104dcecc450876d5d5312b248d4a
Rian Lucas <rianluccas08@gmail.com>
2022-12-12 21:53:44 -0300

    Iniciando SideBar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 export function Sidebar() {
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
-            <span>
+            <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
                 Cronograma de aulas
             </span>
         </aside>
