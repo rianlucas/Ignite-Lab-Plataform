b582678321e8f42d2e0de6c55668f27a1628a6db
Rian Lucas <rianluccas08@gmail.com>
2022-12-12 21:19:22 -0300

    Criando page para os components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { gql, useQuery } from '@apollo/client'
-import { useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import { Header } from './components/Header'
-import { Lesson } from './components/Lesson'
-import { Sidebar } from './components/Sidebar'
-import { Video } from './components/Video'
-import { client } from './lib/apollo'
+import Event from "./pages/Event"
+
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Sidebar/>
-      <Lesson/>
-      <Video/>
+      <Event/>
     </div>
   )
 }
