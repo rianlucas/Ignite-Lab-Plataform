a236d429e613bb03f2931b5d8a20e3458c7e02f5
Rian Lucas <rianluccas08@gmail.com>
2022-12-15 19:54:45 -0300

    Alocando espaço para player de vídeo e criando botões

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -27,7 +27,7 @@ export function Lesson(props: LessonProps) {
                         <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
                         <CheckCircle size={20}/>
                         Conteudo liberado
-                    </span>): (
+                    </span>) : (
                         <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
                         <Lock size={20}/>
                         Em Breve
